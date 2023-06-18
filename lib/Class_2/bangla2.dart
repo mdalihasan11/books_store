141208eabb071c8bc1d667e1abc4c432e170ffd4
pdf_reder <md.alihasan876@gmail.com>
2023-06-18 20:19:38 +0600

    first commit

---
+++ lib/Class_2/bangla2.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class Bangla2 extends StatefulWidget {
+  const Bangla2({Key? key}) : super(key: key);
+
+  @override
+  State<Bangla2> createState() => _Bangla2State();
+}
+
+class _Bangla2State extends State<Bangla2> {
+  PdfViewerController pdfViewerController = PdfViewerController();
+  final PdfTextSearchResult _searchResult = PdfTextSearchResult();
+  double zoom = 0.0;
+  TextEditingController controller = TextEditingController();
+  int pageNo = 0;
+
+  jumpTo(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Enter page No to jump'),
+            content: TextField(
+              controller: controller,
+              keyboardType: TextInputType.text,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],
+              onChanged: (val) {
+                pageNo = int.parse(val);
+              },
+            ),
+            actions: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () {
+                    pdfViewerController.jumpToPage(pageNo);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK'))
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('আমার বাংলা বই'),
+        backgroundColor: Colors.deepPurple,
+        actions: [
+          IconButton(
+              onPressed: () {
+                jumpTo(context);
+              },
+              icon: const Icon(Icons.search))
+        ],
+      ),
+      body: SfPdfViewer.asset('asset/class_2/Bangla2.pdf',
+          scrollDirection: PdfScrollDirection.horizontal),
+    );
+  }
+}
--- lib/VerSon.dart
+++ lib/VerSon.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:new_view/primary.dart';
 import 'package:new_view/stepstone.dart';
 
 class home extends StatefulWidget {
@@ -13,41 +12,36 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: const Text('পাঠ্যপুস্তক'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const steps()));
+        },
+        child: CircleAvatar(
+          radius: 90,
           backgroundColor: Colors.deepPurple,
-          title: const Text('পাঠ্যপুস্তক'),
-          centerTitle: true,
+          child: Image.asset('asset/bangla.PNG'),
         ),
-        body: Container(
-          padding: const EdgeInsets.only(left: 50),
-          child: Center(
-              child: Row(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const steps()));
-                },
-                child: CircleAvatar(
-                  radius: 90,
-                  backgroundColor: Colors.deepPurple,
-                  child: Image.asset('asset/bangla.PNG'),
-                ),
-              ),
-              const SizedBox(width: 15),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const primary()));
-                },
-                child: CircleAvatar(
-                  radius: 90,
-                  backgroundColor: Colors.deepPurple,
-                  child: Image.asset('asset/english.PNG'),
-                ),
-              ),
-            ],
-          )),
-        ));
+      )),
+      // const SizedBox(width: 15),
+      // Center(
+      //     child: GestureDetector(
+      //   onTap: () {
+      //     Navigator.push(context,
+      //         MaterialPageRoute(builder: (context) => const primary()));
+      //   },
+      //   child: CircleAvatar(
+      //     radius: 90,
+      //     backgroundColor: Colors.deepPurple,
+      //     child: Image.asset('asset/english.PNG'),
+      //   ),
+      // )),
+    );
   }
 }
