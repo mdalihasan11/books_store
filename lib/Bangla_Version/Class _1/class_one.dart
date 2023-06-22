96e8930b4ffcdfd855c5f536c400477e54eb9377
pdf_reder <md.alihasan876@gmail.com>
2023-06-22 16:23:14 +0600

    frist commit

---
+++ lib/Bangla_Version/Class _1/class_one.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:new_view/Bangla_Version/Class%20_1/English.dart';
+import 'package:new_view/Bangla_Version/Class%20_1/bangla.dart';
+import 'package:new_view/Bangla_Version/Class%20_1/math.dart';
+
+class class_one extends StatefulWidget {
+  const class_one({Key? key}) : super(key: key);
+
+  @override
+  State<class_one> createState() => _class_oneState();
+}
+
+class _class_oneState extends State<class_one> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("প্রথম শ্রেণি"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 250.0, left: 15.0),
+          child: ListView(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.all(8),
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                        height: 150,
+                        width: 100,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Bangla()));
+                          },
+                          child: Image.asset(
+                              'asset/bangla_version/class_1/bangla.PNG',
+                              width: 300,
+                              height: 150,
+                              fit: BoxFit.fill),
+                        )),
+                    const SizedBox(width: 15),
+                    SizedBox(
+                        height: 150,
+                        width: 100,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const English()));
+                          },
+                          child: Image.asset(
+                              'asset/bangla_version/class_1/English.PNG',
+                              width: 300,
+                              height: 150,
+                              fit: BoxFit.fill),
+                        )),
+                    const SizedBox(width: 15),
+                    SizedBox(
+                        height: 150,
+                        width: 100,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepPurple,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Math()));
+                          },
+                          child: Image.asset(
+                              'asset/bangla_version/class_1/Math.PNG',
+                              width: 300,
+                              height: 150,
+                              fit: BoxFit.fill),
+                        )),
+                  ],
+                ),
+              ]),
+        ));
+  }
+}
